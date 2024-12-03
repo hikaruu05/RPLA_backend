77250e3c00638d92c65320547418936c1865551d
Hikaru Jones <hikaru.elbona.jones@gmail.com>
2024-12-04 01:36:10 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const registerRouter = require('./routes/register');
+const db = mongoose.connection;
+const PORT = process.env.PORT || 5010;
+const app = express();
+app.use('/api', registerRouter);
+app.use(express.json());
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('strictQuery', true);
+
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
+
